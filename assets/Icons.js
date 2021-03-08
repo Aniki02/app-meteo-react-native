65132d217c2d7ab1a8fe2a4136a3b00233bcd233
aniki02 <brahim.zitoun@crisalid.com>
2021-03-08 09:54:28 +0100

    Ajout des icones

---
+++ assets/Icons.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {Icon} from '@ui-kitten/components'
+
+export const BackIcon = (props) => (
+    <Icon {...props} name='arrow-back' />
+);
+
+export const SaveIcon = (props) => (
+    <Icon {...props} name={'save'}/>
+)
+
+export const SaveIconEmpty = (props) => (
+    <Icon {...props} name={'save-outline'}/>
+)
+
+export const ViewedIcon = (props) => (
+    <Icon {...props} name='eye' />
+);
+
+export const ViewedIconEmpty = (props) => (
+    <Icon {...props} name='eye-outline' />
+);
+
+export const HomeIcon = (props) => (
+    <Icon {...props} name='home' />
+);
+
+export const renderItemIcon = (props) => (
+    <Icon {...props} name='person'/>
+);
+
+export const SearchIcon = (props) => (
+    <Icon {...props} name='search'/>
+);
+
+
